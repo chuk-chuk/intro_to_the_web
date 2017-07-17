d99f1076c91fcb2e74b4598f667ca2d37f0c79ab
chuk-chuk <yulia.v.litvin@gmail.com>
2017-07-17 15:35:19 +0100

    first commit

---
+++ app.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+
+set :session_secret,'super secret'
+
+get '/' do
+  "Goodbye World"
+end
+
+
+get '/home' do
+  "Moved to the new page"
+end
+
+get '/about' do
+  "About us"
+end
+
+get '/cat' do
+  "<div>
+    <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
+  </div>"
+end
