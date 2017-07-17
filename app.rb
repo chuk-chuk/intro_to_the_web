ba62016ae2f5a258ffffa7c7897fc022706a6004
Toby Hamand <tobyhamand@gmail.com>
2017-07-17 15:43:01 +0100

    added purple border

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ get '/about' do
 end
 
 get '/cat' do
-  "<div>
+  "<div style='border: 10px dotted purple'>
     <img src='http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png'>
   </div>"
 end
